b71fa1b85edf707d0a3c4169996daa6e2b24022e
josh <josh.kinman@gmail.com>
2014-12-21 12:33:48 -0600

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,26 @@
 ## Calculates inverse of matrix and stores value
-## If inverse is needed again, the value will be retrieved from cache and not calulated again
+## If inverse is needed again, the value will be retrieved from cache and not calculated again
 
-
+##makeCacheMatrix makes special matrix containing a function
+##Sets and gets value of matrix
+##sets ang gets value of inverse os matrix
 makeCacheMatrix <- function(x=matrix()) {
   m <- NULL
-  set <- function(y) {
+  set <- function(y) {   ##set value of matrix
     x <<- y
     m <<- NULL
   }
   get <- function() x
-  setInvMat <- function(inverse) m <<-inverse
+  setInvMat <- function(inverse) m <<-inverse  
   getInvMat <- function() m
   list(set = set, get = get,
        setInvMat = setInvMat,
        getInvMat = getInvMat)
   
 }
+
 ## cacheSolve calculates the inverse of matrix returned by makeCacheMatrix
 ## if inverse has already been calculated, it will be retrieved from the cache
-
-
 cacheSolve <- function(x, ...) {
   m <- x$getInvMat()           ##retrieves value from makeCacheMatrix
   if ( ! is.null(m)) {          ##checks to see if m contains data
